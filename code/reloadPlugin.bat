52a6e0b63f2330d7961315c57618ed955b71f981
Quentin Masingarbe <q.masingarbe@gmail.com>
2020-06-13 16:25:24 +0200

    Better taskkill command

--- code/reloadPlugin.bat
+++ code/reloadPlugin.bat
@@ -2,7 +2,7 @@ rem This little bash script (for windows) will enable you to test your code quic
 rem 1. copy python file in development folder to install folder of rv
 copy C:\path\to\python\file\in\ide\my_code.py C:\path\to\rv\plugin\directory\my_code.py
 rem 2. kill any running instance of RV
-taskkill \IM rv.exe
+taskkill /F /IM rv.exe /T
 rem 3. launch a new instance of RV (with custom env or test file loading)
 rem Please change the pasth to your RV executable
 start "" "C:\Program Files\Shotgun\RV-7.1.1\bin\rv.exe"
